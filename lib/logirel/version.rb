c380bc3ece8b711b2f25c96104cac20cab8afb9b
Olov Sikstrom <sikstrom.olov@hotmail.com>
2011-06-10 14:11:16 +0200

    moved class definition to begin block

--- lib/logirel/version.rb
+++ lib/logirel/version.rb
@@ -2,15 +2,13 @@ module Logirel
   begin
     require 'semver'
     VERSION = SemVer.find.format "%M.%m.%p"
+	class Version < SemVer
+      def parse_numeric(str)
+	    str.scan(/(\d{1,5}).?/).flatten.collect{|x| x.to_i}
+	  end
+    end
   rescue LoadError => e
     puts 'First time installing, eh? Just run "bundle install", unless this is you running it! :) :) :)'
-	VERSION = "0.0.0"
-  end
-  
-  
-  class Version < SemVer
-    def parse_numeric(str)
-	  str.scan(/(\d{1,5}).?/).flatten.collect{|x| x.to_i}
-	end
+    VERSION = "0.0.0"
   end
 end
