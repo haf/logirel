7787d2c96212edbbc3a99d112f27ae66a3b73262
Henrik <henrik@haf.se>
2011-08-12 11:40:22 +0200

    Release 0.1.6. Tested and ready for the world!

--- lib/logirel/cli.rb
+++ lib/logirel/cli.rb
@@ -86,7 +86,7 @@ module Logirel
         build_dep.push "nugets"
 
         if BoolQ.new("Create 'push to nuget server'-task?").exec then
-          pushes = { :depends => to_package.collect { |p| :"#{p[:ruby_key]}_nuget_push" } }
+          pushes = { :depends => [:"env:release"].concat(to_package.collect { |p| :"#{p[:ruby_key]}_nuget_push" }) }
           append_to_file BUILD_FILE, "task :publish#{ inject_dependency pushes }\n"
           to_package.each { |p| nugetpush_task p }
         end
