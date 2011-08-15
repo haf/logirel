b8decfe5cb3d79641a2cf75a00897c7df41a97a5
Henrik <henrik@haf.se>
2011-08-15 22:20:09 +0200

    added samples

---
+++ spec/sample-data/src/Logirel.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Logirel.ConsoleApp
+{
+	class Program
+	{
+		internal const string CONSTANT_MESSAGE = "Hello from ConsoleApp";
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine(CONSTANT_MESSAGE);
+		}
+	}
+}
---
+++ spec/sample-data/src/Logirel.Mvc2App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Logirel.Mvc2App.MvcApplication" Language="C#" %>
